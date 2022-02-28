1e815bcae57c2afab4dac986b2a8aace937efa96
Paweł Grajkowski <grajpawel@gmail.com>
2022-02-28 18:05:50 +0100

    Proper cleanup

--- KeePass.Test/ExampleTest.cs
+++ KeePass.Test/ExampleTest.cs
@@ -1,7 +1,6 @@
-﻿using FluentAssertions;
+﻿namespace KeePass.Test;
 
-namespace KeePass.Test;
-
+using FluentAssertions;
 using KeePass.Forms;
 using KeePassLib.Security;
 using Xunit;
@@ -11,15 +10,15 @@ public class ExampleTest
     public void GivenNullValues_WhenCreatingFpField_ThenFpFieldHasEmptyStrings()
     {
         // Arrange
-        FpField expectedField = new FpField(string.Empty, new ProtectedString(), string.Empty);
+        var expectedField = new FpField(string.Empty, new ProtectedString(), string.Empty);
         
         // Act
-        FpField field = new FpField(null, new ProtectedString(), null);
+        var field = new FpField(null, new ProtectedString(), null);
         
         // Assert
         field.Should().BeEquivalentTo(expectedField);
         
-        /* Does the same thing
+        /* Does the same thing as the line above
         Assert.Equal(string.Empty, field.Group);
         Assert.Equal(string.Empty, field.Name);
         Assert.Equal(string.Empty, field.Value.ReadString());
